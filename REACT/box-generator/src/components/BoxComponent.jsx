208f03ef8e2be0a108e1d60ed4ac19d2886c8124
NaderCP <naderbm@outlook.com>
2025-02-21 20:44:50 +0100

    delete button update

--- REACT/box-generator/src/components/BoxComponent.jsx
+++ REACT/box-generator/src/components/BoxComponent.jsx
@@ -1,18 +1,21 @@
 import React from 'react'
 
 const BoxComponent = ({box}) => {
-
+const deleteBox = (index) =>{
+  const element = document.getElementById(index);
+element.remove();
+}
 //const box = props;
   return (
     <>
     <div>BoxComponent</div>
     <div className='flex flex-row'>
         {box.map((box,index)=>(
-          <div key={index} className='m-3' style={{
+          <div key={index} id={index} className='m-3' style={{
             width: `${box.size}px`,
               height: `${box.size}px`,
               backgroundColor : box.color,         }}>
-
+            <div ><button onClick= {()=>deleteBox(index)} className='"text-red-500 bg-transparent text-xs rounded-full hover:bg-gray-200 focus:outline-none" ' >x</button></div>
           </div>
         ))}
     </div>
